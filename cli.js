d9fe561fb740aa99d6a9385db4f081cc86f7117b
Jonathan Sanchez Pando <jonathansanchez@me.com>
2017-09-23 22:18:37 +0200

    fix cli

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ var proc = require('child_process');
 var path = require('path');
 var electron = require('electron');
 
-var args = [path.join(__dirname, '..')].concat(process.argv.slice(2));
+var args = [path.join(__dirname, '.')].concat(process.argv.slice(2));
 
 var child = proc.spawn(electron, args, {stdio: 'inherit'});
 child.on('close', function (code) {
